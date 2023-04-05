538519915439f6f7efba5bbf5f08db435466fc5a
ayeshafirdaus21@gmail.com <ayeshafirdaus21@gmail.com>
2023-04-05 17:15:11 +0530

    commit -m "day2 up to date"

---
+++ src/MinimizeMaximumofArray.java
@@ -0,0 +1,14 @@
+public class MinimizeMaximumofArray {
+    static int minimizeArrayValue(int[] A) {
+        long sum = 0, res = 0;
+        for (int i = 0; i < A.length; ++i) {
+            sum += A[i];
+            res = Math.max(res, (sum + i) / (i + 1));
+        }
+        return (int)res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minimizeArrayValue(new int []{3,7,1,6}));
+    }
+}
